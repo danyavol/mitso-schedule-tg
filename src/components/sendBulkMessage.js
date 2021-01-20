9ff152a4b35d226ca31a906fba58d913f9fc0d40
Volosyuk Daniil <danya.volosyuk@gmail.com>
2021-01-20 16:16:59 +0300

    sendBulkMessage improve

--- src/components/sendBulkMessage.js
+++ src/components/sendBulkMessage.js
@@ -1,5 +1,6 @@
 const MAX_MSG = 30; // максимальное количество сообщений за раз
 const DELAY = 1000; // задержка между отправкой MAX_MSG сообщений
+const previousMessages = [];
 
 const { deleteUser } = require('../database/usersCollection')
 const sleep = require('./sleep');
@@ -15,7 +16,16 @@ const sleep = require('./sleep');
  */
 module.exports = async (bot, messages) => {
 	for(let i = 0; i < messages.length; i++) {
-		if (i % MAX_MSG === 0) await sleep(DELAY);
+		// Механизм ограничения отправки максимум MAX_MSG сообщений за DELAY миллисекунд
+		if (previousMessages.length < MAX_MSG) previousMessages.push(Date.now());
+		else {
+			previousMessages.shift();
+			previousMessages.push(Date.now());
+		}
+		if (previousMessages.length >= MAX_MSG && Date.now() - previousMessages[0] < DELAY) await sleep(DELAY);
+
+
+		// Отправка сообщения
 		bot.telegram.sendMessage(messages[i].userId, messages[i].msg).catch(err => {
 			if (!err.response.ok && err.code === 403) {
 				// Пользователь заблокировал бота, удаляем его из БД
