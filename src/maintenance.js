c3b3fd4d047c466cf002c3a3aa321e7deb87a1ba
Volosyuk Daniil <danya.volosyuk@gmail.com>
2021-05-17 20:00:47 +0300

    Added admin panel

--- src/maintenance.js
+++ src/maintenance.js
@@ -6,15 +6,30 @@ module.exports = (ctx, next) => {
         let command = ctx.message.text;
 
         switch (command) {
-            case '/stopmsg': 
-                ctx.reply(`Maintenance mod: ${process.env.MAINTENANCE}  =>  1`);
+            case '/admin':
+                ctx.reply(getAdminCommandList());
+                return;
+            case '/maintenance_start': 
+                ctx.reply(`Maintenance mod: ${getMaintenVal(process.env.MAINTENANCE)}  =>  On`);
                 process.env.MAINTENANCE = 1;
                 return;
-            case '/startmsg': 
-                ctx.reply(`Maintenance mod: ${process.env.MAINTENANCE}  =>  0`);
+            case '/maintenance_stop': 
+                ctx.reply(`Maintenance mod: ${getMaintenVal(process.env.MAINTENANCE)}  =>  Off`);
                 process.env.MAINTENANCE = 0;
                 return;
         }
     }
     next();
+
+    function getMaintenVal(val) {
+        if (val == 0) return 'Off'
+        else return 'On';
+    }
+
+    function getAdminCommandList() {
+        let str = '🖥 Панель администратора:\n\n';
+        str += `Режим техобслуживания - ${getMaintenVal(process.env.MAINTENANCE)}\n\n`;
+        str += `/maintenance_start - включить техобслуживание\n/maintenance_stop - выключить техобслуживание`;
+        return str;
+    }
 }
